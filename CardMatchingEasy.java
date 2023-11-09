6dbd27c991f9370a09f70911701db3cd277cacc9
son0210 <105830624+son0210@users.noreply.github.com>
2023-11-09 17:12:07 +0900

    Add files via upload
    화면 크기에 따른 카드 잘림 현상 수정
    점수.... 그리고 화질에 대한 연구 클릭 딜레이 등...

--- CardMatchingEasy.java
+++ CardMatchingEasy.java
@@ -1,3 +1,5 @@
+package Ingame;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -7,17 +9,20 @@ import java.util.List;
 
 public class CardMatchingEasy extends JFrame {
     private JPanel cardPanel;
-    private JPanel topPanel;  // 타이머와 점수를 담을 패널
-    private JLabel timerLabel;  // 타이머 표시 레이블
-    private JLabel scoreLabel;  // 점수 표시 레이블
+    private JPanel topPanel;  
+    private JLabel timerLabel;  
+    private JLabel scoreLabel;  
     private List<Card> cards;
     private Card selectedCard = null;
     private int pairsFound = 0;
+    private static final String initialImagePath = "cardlogo.jpg";
+    private static final int initialImageWidth = 150;
+    private static final int initialImageHeight = 200;
 
     public CardMatchingEasy() {
         setTitle("엎어라 뒤집어라");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(768, 1024);
+        setSize(1200, 900);
         setLocationRelativeTo(null);
         setLayout(new BorderLayout());
 
@@ -28,17 +33,19 @@ public class CardMatchingEasy extends JFrame {
         topPanel.add(timerLabel);
         topPanel.add(scoreLabel);
         topPanel.setBackground(new Color(125, 159, 104));
-        add(topPanel, BorderLayout.NORTH); // 상단에 타이머와 점수 패널 추가
+        add(topPanel, BorderLayout.NORTH);
+
 
         cardPanel = new JPanel(new GridLayout(4, 4, 10, 10));
         cards = new ArrayList<>();
 
         for (int i = 0; i < 8; i++) {
-            String imagePath = "easy/easy" + (i+1) + ".jpg";
+            String imagePath = "easy/easy" + (i + 1) + ".jpg";
             ImageIcon icon = new ImageIcon(imagePath);
 
             for (int j = 0; j < 2; j++) {
                 Card card = new Card(icon);
+                card.addMouseListener(new CardClickListener());
                 cards.add(card);
             }
         }
@@ -46,42 +53,52 @@ public class CardMatchingEasy extends JFrame {
         Collections.shuffle(cards);
 
         for (Card card : cards) {
-            card.addActionListener(new CardClickListener());
             cardPanel.add(card);
         }
 
-        add(cardPanel, BorderLayout.CENTER);
+        initializeCardImages();
+
+        JPanel centerPanel = new JPanel(new FlowLayout());
+        centerPanel.add(cardPanel);
+
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        mainPanel.add(topPanel, BorderLayout.NORTH);
+        mainPanel.add(centerPanel, BorderLayout.CENTER);
+        
+        add(mainPanel);
 
         setVisible(true);
     }
 
-    public class Card extends JButton {
+    private void initializeCardImages() {
+        ImageIcon defaultIcon = new ImageIcon(initialImagePath);
+        Image img = defaultIcon.getImage().getScaledInstance(initialImageWidth, initialImageHeight, Image.SCALE_SMOOTH);
+        defaultIcon = new ImageIcon(img);
+        
+        for (Card card : cards) {
+            card.setIcon(defaultIcon);
+        }
+    }
+
+    public class Card extends JLabel {
         private ImageIcon icon;
-        private Color defaultBackgroundColor = new Color(237, 227, 206);
         private boolean isFaceUp = false;
 
         public Card(ImageIcon icon) {
             this.icon = icon;
-            setPreferredSize(new Dimension(150, 200)); // 버튼의 크기를 설정
-            setBackground(defaultBackgroundColor);
+            setPreferredSize(new Dimension(initialImageWidth, initialImageHeight));
+            setIcon(icon);
         }
 
         public void showCard() {
             isFaceUp = true;
-            setIcon(scaleIcon(icon, getPreferredSize())); // 이미지 아이콘 크기를 버튼의 크기에 맞게 조절
-            setBackground(new Color(237, 227, 206)); // 배경색을 원하는 색으로 설정
-        }
-
-        public void hideCard() {
-            isFaceUp = false;
-            setIcon(null);
-            setBackground(defaultBackgroundColor);
+            setIcon(scaleIcon(icon, getPreferredSize()));
         }
 
         public boolean isFaceUp() {
             return isFaceUp;
         }
-
+        
         private ImageIcon scaleIcon(ImageIcon originalIcon, Dimension size) {
             Image image = originalIcon.getImage();
             Image scaledImage = image.getScaledInstance(size.width, size.height, Image.SCALE_SMOOTH);
@@ -89,9 +106,9 @@ public class CardMatchingEasy extends JFrame {
         }
     }
 
-    private class CardClickListener implements ActionListener {
+    private class CardClickListener extends MouseAdapter {
         @Override
-        public void actionPerformed(ActionEvent e) {
+        public void mouseClicked(MouseEvent e) {
             Card clickedCard = (Card) e.getSource();
 
             if (!clickedCard.isFaceUp) {
@@ -109,23 +126,30 @@ public class CardMatchingEasy extends JFrame {
                         clickedCard.setEnabled(false);
                         selectedCard = null;
                     } else {
-                        Timer timer = new Timer(400, new ActionListener() {
-                            @Override
-                            public void actionPerformed(ActionEvent e) {
-                                if (selectedCard != null) {
-                                    selectedCard.hideCard();
-                                }
-                                if (clickedCard != null) {
-                                    clickedCard.hideCard();
-                                }
-                                selectedCard = null;
-                            }
-                        });
+                    	Timer timer = new Timer(50, new ActionListener() {
+                    	    @Override
+                    	    public void actionPerformed(ActionEvent e) {
+                    	        if (selectedCard != null) {
+                    	            selectedCard.isFaceUp = false;
+                    	            selectedCard.setIcon(scaleIcon(new ImageIcon(initialImagePath), selectedCard.getPreferredSize()));
+                    	        }
+                    	        if (clickedCard != null) {
+                    	            clickedCard.isFaceUp = false;
+                    	            clickedCard.setIcon(scaleIcon(new ImageIcon(initialImagePath), clickedCard.getPreferredSize()));
+                    	        }
+                    	        selectedCard = null;
+                    	    }
+                    	});
                         timer.setRepeats(false);
                         timer.start();
                     }
                 }
             }
+        }
+        private ImageIcon scaleIcon(ImageIcon originalIcon, Dimension size) {
+            Image image = originalIcon.getImage();
+            Image scaledImage = image.getScaledInstance(size.width, size.height, Image.SCALE_SMOOTH);
+            return new ImageIcon(scaledImage);
         }
     }
 
