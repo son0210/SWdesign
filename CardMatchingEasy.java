f643f7d4da7d39ef410ac08fcdce814c51fcb831
son0210 <105830624+son0210@users.noreply.github.com>
2023-12-01 02:18:55 +0900

    Add files via upload
    타이머 점수 연결 직전까지
    (코드 취합 후 UI 수정)

--- CardMatchingEasy.java
+++ CardMatchingEasy.java
@@ -1,4 +1,4 @@
-package Ingame;
+package Game;
 
 import javax.swing.*;
 import java.awt.*;
@@ -8,67 +8,108 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * 난이도 중의 카드 매칭 게임을 나타냅니다.
+ * 난이도 하의 카드 매칭 게임을 나타냅니다.
  * JFrame을 확장하며 게임의 그래픽 사용자 인터페이스(GUI)를 포함합니다.
  */
-public class CardMatchingEasy extends JFrame {
+public class CardMatchingEasy extends JPanel {
     private JPanel cardPanel;
-    private JPanel topPanel;  
+    private JPanel topPanel;
     private JButton button;
     private JLabel scoreLabel;
     public Score score;
-    public Level1Timer level1timer;
+    public static Level1Timer level1timer;
     private List<Card> cards;
     private int pairsFound = 0;
     private Card selectedCard = null;
     private boolean isComparing = false;
-    private static final String initialImagePath = "src/Image/cardlogo.jpg";
+    private static final String initialImagePath = "src/image/cardlogo.jpg";
+
+    protected MainPage mainPage;
+    private CardLayout cardLayout;
+    private JPanel panel;
+    private JPanel panelG;
+	private String[] data;
 
     /**
      * CardMatchingEasy 클래스의 생성자입니다.
      * 게임 창을 초기화하고 사용자 인터페이스를 설정합니다.
      */
-    public CardMatchingEasy() {
-        setTitle("엎어라 뒤집어라");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setExtendedState(JFrame.MAXIMIZED_BOTH);
-        setMinimumSize(new Dimension(1400, 1100));
-        
+    public CardMatchingEasy(CardLayout layout, JPanel panel, MainPage mainPage, String[] data) {
+        cardLayout = layout;
+        this.panel = panel;
+        this.mainPage = mainPage;
+
+        setVisible(true);
+        setLayout(cardLayout);
+
+        panelG = new JPanel();
+        panelG.setLayout(new GridBagLayout());
+
         score = new Score();
-        scoreLabel = new JLabel("Score: 0");
+        scoreLabel = new JLabel("Score:0");
+        topPanel = new JPanel(new GridBagLayout());
+        topPanel.setBorder(BorderFactory.createEmptyBorder(0, 40, 0, 40));
+        topPanel.setBackground(Utility.maincolor);
         
-        topPanel = new JPanel(new BorderLayout());
-        topPanel.setBackground(new Color(125, 159, 104));
-        topPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-        topPanel.add(scoreLabel, BorderLayout.WEST);
-        
-        // 오른쪽에 버튼 추가
-        ImageIcon pauseIcon = new ImageIcon("src/Image/pause.png"); 
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.WEST;
+        topPanel.add(scoreLabel, gbc);
+
+        // Add some horizontal space between scoreLabel and the button
+        gbc.gridx = 1;
+        gbc.weightx = 1.0;
+        topPanel.add(Box.createHorizontalStrut(10), gbc);
+
+        // Add button to the right side
+        gbc.gridx = 2;
+        gbc.weightx = 0.0;
+        ImageIcon pauseIcon = new ImageIcon("src/image/pause.png");
         button = new JButton(pauseIcon);
         button.setPreferredSize(new Dimension(20, 20));
-        button.setBackground(new Color(125, 159, 104));
+        button.setBackground(Utility.maincolor);
         button.setBorderPainted(false);
-        topPanel.add(button, BorderLayout.EAST);
-        
+        button.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent evt) {
+                addpausePanel(cardLayout, panel, mainPage); }
+            public void mouseEntered(MouseEvent evt) {
+                button.setBackground(Utility.pointcolor); }
+            public void mouseExited(MouseEvent evt) {
+                button.setBackground(Utility.maincolor); } });
+        topPanel.add(button, gbc);
+
         //타이머 추가
         Level1Timer level1timer = new Level1Timer();
         JProgressBar timerVisible = level1timer.getProgressBar();
         topPanel.add(timerVisible);
+
+        // Add the JProgressBar to the center
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 3;
+        gbc.anchor = GridBagConstraints.CENTER;
+        topPanel.add(timerVisible, gbc);
         
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        panelG.add(topPanel, gbc);
+
         cardPanel = new JPanel(new GridLayout(4, 4, 10, 10));
-        cardPanel.setBackground(new Color(237, 227, 206));
+        cardPanel.setBackground(Utility.backcolor);
         cards = new ArrayList<>();
 
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        int cardWidth = (int) (screenSize.getWidth()* 0.1);
-        int cardHeight = (int) (screenSize.getHeight()* 0.2);
-        
+        Dimension parentFrame = Toolkit.getDefaultToolkit().getScreenSize();
+        int cardWidth = (int) (parentFrame.getWidth()* 0.09);
+        int cardHeight = (int) (parentFrame.getHeight()* 0.18);
         for (int i = 0; i < 8; i++) {
-            String imagePath = "src/Image/easy/easy" + (i + 1) + ".jpg";
-            ImageIcon cardIcon = new ImageIcon(imagePath);
+            String imagePath = "src/image/easy/easy" + (i + 1) + ".jpg";
+            ImageIcon icon = new ImageIcon(imagePath);
 
             for (int j = 0; j < 2; j++) {
-                Card card = new Card(cardIcon, cardWidth, cardHeight);
+                Card card = new Card(icon, cardWidth, cardHeight);
                 card.addMouseListener(new CardClickListener());
                 cards.add(card);
             }
@@ -80,19 +121,18 @@ public class CardMatchingEasy extends JFrame {
         for (Card card : cards) {
             cardPanel.add(card);
         }
-        
+
         JPanel centerPanel = new JPanel(new FlowLayout());
-        centerPanel.setBackground(new Color(237, 227, 206));
         centerPanel.add(cardPanel);
-        
-        JPanel mainPanel = new JPanel(new BorderLayout());
-        mainPanel.add(topPanel, BorderLayout.NORTH);
-        mainPanel.add(centerPanel, BorderLayout.CENTER);
-        
-        add(mainPanel);
+        centerPanel.setBackground(Utility.backcolor);
+
+        gbc.gridy = 1;
+        panelG.add(centerPanel,gbc);
+
         setVisible(true);
+        add(panelG,"EasyPanel");
     }
-    
+
     /**
      * 게임의 모든 카드에 대한 이미지를 초기화합니다.
      * 각 카드의 기본 이미지 아이콘을 초기 이미지의 크기에 맞게 조절합니다.
@@ -103,9 +143,11 @@ public class CardMatchingEasy extends JFrame {
         defaultIcon = new ImageIcon(img);
 
         for (Card card : cards) {
+            card.setPreferredSize(new Dimension(cardWidth, cardHeight));
             card.setIcon(defaultIcon);
         }
     }
+    
     /**
      * Card 클래스는 게임에서 사용되는 카드를 나타냅니다.
      * 각 카드에는 연관된 이미지 아이콘이 있으며 앞면이나 뒷면으로 나타낼 수 있습니다.
@@ -141,7 +183,7 @@ public class CardMatchingEasy extends JFrame {
         public boolean isFaceUp() {
             return isFaceUp;
         }
-        
+
         /**
          * 이미지 아이콘을 지정된 크기로 조절하는 유틸리티 메서드입니다.
          *
@@ -149,13 +191,13 @@ public class CardMatchingEasy extends JFrame {
          * @param size 조절된 이미지의 크기를 나타내는 Dimension 객체입니다.
          * @return 지정된 크기로 조절된 이미지 아이콘을 나타내는 ImageIcon 객체입니다.
          */
-        public ImageIcon scaleIcon(ImageIcon originalIcon, Dimension size) {
+        private ImageIcon scaleIcon(ImageIcon originalIcon, Dimension size) {
             Image image = originalIcon.getImage();
             Image scaledImage = image.getScaledInstance(size.width, size.height, Image.SCALE_SMOOTH);
             return new ImageIcon(scaledImage);
         }
     }
-
+    
     /**
      * CardClickListener 클래스는 카드에 대한 마우스 클릭 이벤트를 처리합니다.
      * MouseAdapter 인터페이스를 구현합니다.
@@ -175,6 +217,7 @@ public class CardMatchingEasy extends JFrame {
                 selectedCard = clickedCard;
             } else {
                 isComparing = true;
+
                 Timer cardTimer = new Timer(40, new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
@@ -183,8 +226,8 @@ public class CardMatchingEasy extends JFrame {
                             scoreLabel.setText("Score: " + score.getScore());
                             pairsFound++;
                             if (pairsFound == 8) {
-                            	score.timerScore = level1timer.getTimerValue();
-                            	JOptionPane.showMessageDialog(null, "You win!");
+//                            	score.timerScore = level1timer.getTimerValue();
+                                addfinishPanel(cardLayout, panel, mainPage);
                             }
                             selectedCard.setEnabled(false);
                             clickedCard.setEnabled(false);
@@ -195,38 +238,58 @@ public class CardMatchingEasy extends JFrame {
                             clickedCard.isFaceUp = false;
                             clickedCard.setIcon(scaleIcon(new ImageIcon(initialImagePath), clickedCard.getPreferredSize()));
                         }
-
                         selectedCard = null;
                         isComparing = false;
                     }
                 });
-
                 cardTimer.setRepeats(false);
                 cardTimer.start();
             }
         }
 
-    	
-    	/**
-    	 * 이미지 아이콘을 지정된 크기로 조절하는 유틸리티 메서드입니다.
-    	 *
-    	 * @param originalIcon 조절할 원본 이미지 아이콘입니다.
-    	 * @param size 조절된 이미지의 크기를 나타내는 Dimension 객체입니다.
-    	 * @return 지정된 크기로 조절된 이미지 아이콘을 나타내는 ImageIcon 객체입니다.
-    	 */
-        public ImageIcon scaleIcon(ImageIcon originalIcon, Dimension size) {
+        /**
+         * 이미지 아이콘을 지정된 크기로 조절하는 유틸리티 메서드입니다.
+         *
+         * @param originalIcon 조절할 원본 이미지 아이콘입니다.
+         * @param size 조절된 이미지의 크기를 나타내는 Dimension 객체입니다.
+         * @return 지정된 크기로 조절된 이미지 아이콘을 나타내는 ImageIcon 객체입니다.
+         */
+        private ImageIcon scaleIcon(ImageIcon originalIcon, Dimension size) {
             Image image = originalIcon.getImage();
             Image scaledImage = image.getScaledInstance(size.width, size.height, Image.SCALE_SMOOTH);
             return new ImageIcon(scaledImage);
         }
     }
 
-    /**
-     * CardMatchingEasy 애플리케이션을 시작하는 메인 메서드입니다.
-     *
-     * @param args 명령행 인수 (사용되지 않음).
-     */
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new CardMatchingEasy());
+    private void addpausePanel(CardLayout layout, JPanel panel, MainPage mainPage) {
+        cardLayout = layout;
+        this.panel = panel;
+        this.mainPage = mainPage;
+
+        // 새로운 패널 생성
+        PausePage pausePanel = new PausePage(layout, panel, mainPage);
+
+        // 기존 패널에 새로운 패널 추가
+        panelG.add(pausePanel.getPausePanel(),"pausePanel");
+
+        CardLayout cardLayout = (CardLayout) panelG.getLayout();
+        cardLayout.show(panelG, "pausePanel");
+    }
+    
+    private void addfinishPanel(CardLayout layout, JPanel panel, MainPage mainPage) {
+
+        cardLayout = layout;
+        this.panel = panel;
+        this.mainPage = mainPage;
+
+        // 새로운 패널 생성
+        FinishPage finishPanel = new FinishPage(layout, panel, mainPage);
+
+        // 기존 패널에 새로운 패널 추가
+        panel.add(finishPanel.getFinishPanel(), "finishPanel");
+
+        // 카드를 새로운 패널로 전환
+        cardLayout = (CardLayout) panel.getLayout();
+        cardLayout.show(panel, "finishPanel");
     }
 }
